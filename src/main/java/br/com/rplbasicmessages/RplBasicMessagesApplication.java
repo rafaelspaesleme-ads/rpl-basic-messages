b36cfce1531abc4664d54484a4bfe52114d17cea
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2025-01-15 11:55:56 -0300

    first commit

---
+++ src/main/java/br/com/rplbasicmessages/RplBasicMessagesApplication.java
@@ -0,0 +1,13 @@
+package br.com.rplbasicmessages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RplBasicMessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RplBasicMessagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rplbasicmessages/RplBasicMessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rplbasicmessages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RplBasicMessagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
